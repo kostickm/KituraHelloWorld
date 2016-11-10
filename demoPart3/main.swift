cdab7d42023e490352be0c8334d3d3e07497c5c6
Megan Kostick <mkostick@us.ibm.com>
2016-11-10 15:37:48 -0800

    Create main.swift

---
+++ demoPart3/main.swift
@@ -0,0 +1,28 @@
+import Kitura
+import HeliumLogger
+
+// Initialize HeliumLogger
+HeliumLogger.use()
+
+// Create a new router
+let router = Router()
+
+// Handle HTTP GET requests to /
+router.get("/") {
+    request, response, next in
+    response.send("Hello, World!")
+    next()
+}
+
+// Status route
+router.get("/status") {
+    request, response, next in
+    response.send("OK")
+    next()
+}
+
+// Add an HTTP server and connect it to the router
+Kitura.addHTTPServer(onPort: 8090, with: router)
+
+// Start the Kitura runloop (this call never returns)
+Kitura.run()
