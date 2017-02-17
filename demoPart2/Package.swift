d1f095d1ffa15dad99ed6a0669c29f5e22f652c0
Megan Kostick <mkostick@us.ibm.com>
2017-02-16 16:12:33 -0800

    Update to latest Kitura and HeliumLogger

--- demoPart2/Package.swift
+++ demoPart2/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "myFirstProject",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6)
     ])
