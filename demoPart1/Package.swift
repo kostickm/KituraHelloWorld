4dd1ce81871ccc996cc537b90ecffd535de5f46f
Megan Kostick <mkostick@us.ibm.com>
2017-02-16 16:10:00 -0800

    Update Kitura version

--- demoPart1/Package.swift
+++ demoPart1/Package.swift
@@ -3,5 +3,5 @@ import PackageDescription
 let package = Package(
     name: "myFirstProject",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2)
     ])
